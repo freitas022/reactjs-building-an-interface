20be21803b4fdf8723c1ce69689a1f8d4b6bbb14
Matheus Freitas <mf022jr@gmail.com>
2024-06-17 16:07:08 -0300

    Reviewing array and objects destructuring

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App({name}) {
+
+  const people = ["Math", "Alex", "Maria"];
+  console.log(people[1]);
+  const [, , third] = ["Math", "Alex", "Maria"];
+  // console.log(first);
+  console.log(third);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello {third}</h1>
       </header>
     </div>
   );
