6565b7331dd668e8df1c5cb92d3ee7ab1cfbcfcd
Matheus Freitas <mf022jr@gmail.com>
2024-06-17 17:25:26 -0300

    Sending interactions up components trees

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import './App.css';
 import { useState } from 'react';
+import { FaStar } from "react-icons/fa";
+
+function handleClick() {
+  console.log("Obrigado pela sua avalição.")
+}
+
+const createArray = (lenght) => [
+  ...Array(lenght)
+];
+
+function Star({ selected = false, onSelect }) {
+  return <FaStar color={selected ? "yellow" : "gray"}
+    onClick={onSelect} />;
+}
+
+function StarRating({ totalStars = 5 }) {
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star key={i} selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)} />
+      ))}
+    </>
+  )
+}
 
 function App() {
-  const [status, setStatus] = useState(false);
-  return (
-    <div>
-      <label>Click here to update status</label>
-      <input type="checkbox" value={status} onChange={() => setStatus(!status)} />
-      <p>{status ? "Ok" : "Not implemented"}</p>
-    </div>
-  )
+  return <StarRating totalStars={5} />;
 }
 
 export default App;
