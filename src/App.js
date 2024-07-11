66c3772427a40f94aadaecde95b4def72f86065e
Matheus Freitas <mf022jr@gmail.com>
2024-07-11 17:08:30 -0300

    useReducer part 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import "./App.css";
-import { useEffect } from "react";
 
 function App() {
-  const [data, setData] = useState([]);
 
-  useEffect(() => {
-    fetch(`https://api.github.com/users`)
-      .then((response) => response.json())
-      .then(setData);
-  }, []);
+  const [number, setNumber] = useReducer((number, newNumber) => number + newNumber, 0);
 
   return (
     <>
-    <ul>
-      {data.map((user) => (
-        <div key={user.id}>
-          <li>Login: {user.login} <br /> Url: {user.url}</li>
-        </div>
-      ))}
-    </ul>
-    <button onClick={() => setData([])}>Remover dados</button>
+    <button on onClick={() => setNumber(1)}>{number}</button>
     </>
   );
 }
