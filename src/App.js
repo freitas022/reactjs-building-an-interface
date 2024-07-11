d99256902a95218bf08eb8e89b8090db7c61bc62
Matheus Freitas <mf022jr@gmail.com>
2024-07-11 18:24:56 -0300

    managing  form inputs with useRef

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import { useReducer } from "react";
+import { useRef } from "react";
 import "./App.css";
 
-const initalState = {
-  message: "Olá!",
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "patrao":
-      return {
-        message: "Fala meu patrão!",
-      };
-    case "diretor":
-      return {
-        message: "Fala meu diretor!",
-      };
-  }
-}
-
 function App() {
-  const [state, dispatch] = useReducer(reducer, initalState);
+
+  const sound = useRef();
+  const color = useRef();
+
+  const submit = (e) => {
+    e.preventDefault();
+    const soundVal = sound.current.value;
+    const colorVal = color.current.value;
+
+    alert(`${soundVal} sounds like ${colorVal}`);
+    sound.current.value = "";
+    color.current.value = "";
+  }
 
   return (
     <>
-      <p>Mensagem: {state.message}</p>
-      <button onClick={() => dispatch({type: "diretor"})}>diretor</button>
-      <button onClick={() => dispatch({type: "patrao"})}>patrao</button>
+      <form onSubmit={submit}>
+        <input type="text" ref={sound} placeholder="Sound..." />
+        <input type="color" ref={color} />
+        <button>add</button>
+      </form>
     </>
   );
 }
