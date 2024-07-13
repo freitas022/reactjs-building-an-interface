3c33e0c61ba04eeac417c9efe7859bad4d05c6bb
Matheus Freitas <mf022jr@gmail.com>
2024-07-13 16:06:48 -0300

    create context part 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import { createContext } from "react";
+import { useContext } from "react";
+import { TreeContext } from "../src/index";
 import "./App.css";
-
-export const TreeContext = createContext();
-
-const trees = [
-  { id: 1, type: "Maple" },
-  { id: 2, type: "Oak" },
-  { id: 3, type: "Family" },
-  { id: 4, type: "Component" },
-];
 
 function App() {
 
+  const { trees } = useContext(TreeContext);
+
   return (
     <>
-      <TreeContext.Provider value={{ trees }}>
-        <h1>Árvores: </h1>
-        {trees.map(t => (
-          <p key={t.id}>{t.type}</p>
-        ))}
-      </TreeContext.Provider>
+      <h1>Árvores: </h1>
+      {trees.map(tree => (
+        <li key={tree.id}>{tree.type}</li>
+        ))
+      }
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { createContext } from "react";
+
+export const TreeContext = createContext();
+
+const trees = [
+  { id: 1, type: "Maple" },
+  { id: 2, type: "Oak" },
+  { id: 3, type: "Family" },
+  { id: 4, type: "Component" },
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <TreeContext.Provider value={{ trees }}>
     <App />
-  </React.StrictMode>
+  </TreeContext.Provider>
 );
